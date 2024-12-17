4430e0ddbedd1606acaee956a866c83eaa8cb2b6
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2024-12-17 20:32:41 +0100

    Update elfextractor.py
    updated extract_elf_header_info function.
    + renamed like readelf -h xyz.elf

--- ELFextractor/elfextractor.py
+++ ELFextractor/elfextractor.py
@@ -40,24 +40,27 @@ def extract_elf_header_info(elf_file):
     # Extract ELF header information
     eheader = elf_file.header
     header_info = {
-        "File Type": str(eheader.file_type),
-        "Machine Type": str(eheader.machine_type),
-        "Entry Point": hex(eheader.entrypoint),
-        "Program Header Offset": eheader.program_header_offset,
-        "Section Header Offset": eheader.section_header_offset,
-        "Section Header Offset": eheader.section_header_offset,
-        "Program Header Offset": eheader.program_header_offset,
-        "Processor Flags": eheader.processor_flag,
-        "Header Size": eheader.header_size,
-        "Program Header Size": eheader.program_header_size,
-        "Number of Program Headers": eheader.numberof_segments,
-        "Section Header Size": eheader.section_header_size,
-        "Number of Section Headers": eheader.numberof_sections,
-        "Section Header String Table Index": eheader.section_name_table_idx,
-        "ELF Class": str(eheader.identity_class),
-        "ELF Data": str(eheader.identity_data)
+        #"Magic": str(hex(eheader.identity)),
+        "Magic": ' '.join(f'{byte:02x}' for byte in eheader.identity),
+        "Class": str(eheader.identity_class.name),
+        "Data": str(eheader.identity_data.name),
+        "ELF Version": eheader.identity_version.name, # The ELF specification version (current version is 1).
+        "OS/ABI": eheader.identity_os_abi.name,
+        "ABI Version": eheader.identity_abi_version,
+        "Type": str(eheader.file_type.name),
+        "Machine": str(eheader.machine_type.name),
+        "Version": eheader.object_file_version.name, #readelf 0x1 but mine is CURRENT, Check!
+        "Entry point address": hex(eheader.entrypoint),
+        "Start of program headers": eheader.program_header_offset,
+        "Start of section headers": eheader.section_header_offset,    
+        "Flags": hex(eheader.processor_flag),
+        "Size of this header": eheader.header_size,
+        "Size of program headers": eheader.program_header_size,        
+        "Number of program headers": eheader.numberof_segments,        
+        "Size of section headers": eheader.section_header_size,
+        "Number of section headers": eheader.numberof_sections,
+        "Section header string table index": eheader.section_name_table_idx        
     }
-    
     # Add header information to the ELF data dictionary
     elf_data.update(header_info)
 

0f8a34f56d9650654af4e08de17f17862abead2b
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-02-08 00:44:17 +0100

    Update ELFxtractor_via_Lief.py
    -- extract_elf_file_header_info

--- ELF Extractor via LIEF/ELFxtractor_via_Lief.py
+++ ELF Extractor via LIEF/ELFxtractor_via_Lief.py
@@ -16,7 +16,7 @@ from elf_metadata import detect_architecture
 
 initflag = True
 
-
+from elf_file_header import extract_elf_file_header_info
     
 def clear_log_file(log_file_path) -> bool:
     try:
@@ -227,37 +227,6 @@ def extract_elf_file_notes_info(elf) -> dict:
     except Exception as e:
         eprint(f"An unexpected error occurred: {e}")
     return {"owner" : owner, "build_id" : build_id, "abi_version" : abi_version}
-
-# Extract ELF header information ##Checked: readelf  --file-header sample.elf
-def extract_elf_file_header_info(elf_file) -> dict:
-    # Extract ELF header information
-    eheader = elf_file.header
-    header_info = {
-        #"Magic": str(hex(eheader.identity)),
-        "Magic": ' '.join(f'{byte:02x}' for byte in eheader.identity),
-        "Class": str(eheader.identity_class.name),
-        "Data": str(eheader.identity_data.name),
-        "ELF Version": eheader.identity_version.name, # The ELF specification version (current version is 1).
-        "OS/ABI": eheader.identity_os_abi.name,
-        "ABI Version": eheader.identity_abi_version,
-        "Type": str(eheader.file_type.name),
-        "Machine": str(eheader.machine_type.name),
-        "Version": eheader.object_file_version.name, #readelf 0x1 but mine is CURRENT, Check!
-        "Entry point address": hex(eheader.entrypoint),
-        "Start of program headers": eheader.program_header_offset,
-        "Start of section headers": eheader.section_header_offset,    
-        "Flags": hex(eheader.processor_flag),
-        "Size of this header": eheader.header_size,
-        "Size of program headers": eheader.program_header_size,        
-        "Number of program headers": eheader.numberof_segments,        
-        "Size of section headers": eheader.section_header_size,
-        "Number of section headers": eheader.numberof_sections,
-        "Section header string table index": eheader.section_name_table_idx        
-    }
-    # Add header information to the ELF data dictionary
-    #print(header_info)
-    #elf_data.update(header_info)
-    return header_info
 
 #Section Headers  ##Checked  readelf --section-headers sample.elf
 def extract_elf_section_headers_info(elf_file) -> dict:

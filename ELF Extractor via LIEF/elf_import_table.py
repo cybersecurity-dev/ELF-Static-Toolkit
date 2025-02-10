dc21ccd0a67e65472e11719d07ab0f8ee68f86bd
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-02-10 12:36:32 +0100

    Update elf_import_table.py
    fixed import table version error

--- ELF Extractor via LIEF/elf_import_table.py
+++ ELF Extractor via LIEF/elf_import_table.py
@@ -1,10 +1,17 @@
+import lief
+
 #nm -C -D <file> | grep -w U | awk '{print $2}'
 def extract_elf_import_info(elf_file, fpath):
     imported_funcs = {}
     
     for entry in elf_file.imported_symbols:
         if entry.name:
-            version = entry.symbol_version.name if entry.has_version else "UNKNOWN"
-            imported_funcs[f"{version}_{entry.name}"] = 1    
-    
+            version = "Unknown"
+            if entry.has_version:
+                symbol_version = entry.symbol_version
+                if isinstance(symbol_version, lief.ELF.SymbolVersionAux):
+                    version = symbol_version.name
+                elif isinstance(symbol_version, lief.ELF.SymbolVersionAuxRequirement):
+                    version = symbol_version.name
+            imported_funcs[f"{version}_{entry.name}"] = 1
     return imported_funcs

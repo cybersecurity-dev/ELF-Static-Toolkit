d4ad269bac4dffa3f2410e6c8a574a7c1f44cb79
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-02-10 12:31:26 +0100

    Update elf_metadata.py
    ++get_elf_file_type
    ++get_abi_version
    ++get_build_id
    ++extract_elf_file_notes_info

--- ELF Extractor via LIEF/elf_metadata.py
+++ ELF Extractor via LIEF/elf_metadata.py
@@ -26,6 +26,14 @@ def shannon_entropy(data: bytes) -> float:
             entropy -= p * math.log2(p)
     return entropy
 
+def get_elf_file_type(fpath):
+    """Gets the ELF file type of an ELF file."""
+    binary = lief.parse(fpath)
+    if binary is None:
+        raise Exception(f"Failed to parse:{fpath}")
+        return None
+    return binary.header.file_type.name
+
 # checks if the current user has execute permissions 
 def has_execute_permissions (fpath):
     return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
@@ -64,3 +72,39 @@ def detect_architecture(fpath) -> str:
         return "IA-64 (Itanium)"
     else:
         return f"Unknown ELF architecture ({machine_type})"
+
+def get_abi_version(elf):
+    """Extracts the Build ID from an ELF binary using LIEF."""
+    for note in elf.notes:
+        #if note.name == "GNU" and note.type == lief.ELF.NOTE_TYPES.ABI_TAG:
+        if note.name == "GNU" and note.type == 1:
+            abi_version = (note.description[4], note.description[8], note.description[12])
+            note_owner = note.name
+            note_abi_version = '.'.join(map(str, abi_version[:3]))
+            return note_owner, note_abi_version
+    return None, None
+
+def get_build_id(elf):
+    """Extracts the ABI tag from an ELF binary using LIEF."""
+    for note in elf.notes:
+        #if  note.name == "GNU" and note.type == lief.ELF.NOTE_TYPES.GNU_BUILD_ID:
+        if  note.name == "GNU" and note.type == 3:
+            note_build_id = ''.join(f"{byte:02x}" for byte in note.description) if note.description else "None"
+            #note_build_id = note.description.hex()
+            note_owner = note.name
+            return note_owner, note_build_id
+    return None, None
+
+def extract_elf_file_notes_info(elf, fpath) -> dict:
+    owner = None
+    build_id = None
+    abi_version = None
+    try:
+        owner, build_id = get_build_id(elf)
+        owner, abi_version = get_abi_version(elf)
+    except lief.exception as lief_error:
+        print(f"LIEF Error: {lief_error}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        print(f"File:{fpath}")
+    return {"owner" : owner, "build_id" : build_id, "abi_version" : abi_version}

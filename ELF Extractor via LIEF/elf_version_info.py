2d2ac67c0010db72363ca060cf51d0ac2fcba73a
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-02-23 18:19:43 +0100

    Update elf_version_info.py
    more effective data extraction

--- ELF Extractor via LIEF/elf_version_info.py
+++ ELF Extractor via LIEF/elf_version_info.py
@@ -1,13 +1,15 @@
 
 # Function to extract information from the version requirement section 
-# ##Checked readelf --version-info  sample.elf
+# Checked readelf --version-info  sample.elf
+# .gnu.version_r
 def extract_elf_file_version_needs(elf_file) -> dict:
     # Extract version needs section information
     version_needs_info = {}
     for version_need in elf_file.symbols_version_requirement:
-        for aux_idx, aux in enumerate(version_need.get_auxiliary_symbols()):
-            #print(aux)
-            version_needs_info[f"{aux_idx}_version_{version_need.version}_needs_{version_need.name}_name"] = aux.name
-            version_needs_info[f"{aux_idx}_version_{version_need.version}_needs_{version_need.name}_flag"] = aux.flags
-            version_needs_info[f"{aux_idx}_version_{version_need.version}_needs_{version_need.name}_version"] = aux.other    
+        version_needs_info[f"{version_need.name}"] = 1       
+        #Class which represents an entry defined in DT_VERDEF or .gnu.version_d
+        for aux in version_need.get_auxiliary_symbols():
+            version_needs_info[f"{version_need.name}_{aux.name}"] = 1
+            version_needs_info[f"{version_need.name}_{aux.name}_flag"] = aux.flags
+            version_needs_info[f"{version_need.name}_{aux.name}_version"] = aux.other
     return version_needs_info

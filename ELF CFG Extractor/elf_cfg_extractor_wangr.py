2dc580dbcd256c1fbb0d5b181b1a8ee41cb9777c
cybersecurity-dev <174958202+cybersecurity-dev@users.noreply.github.com>
2025-12-14 19:18:45 +0100

    small fix

--- ELF CFG Extractor/elf_cfg_extractor_wangr.py
+++ ELF CFG Extractor/elf_cfg_extractor_wangr.py
@@ -7,37 +7,38 @@ def save_nxgraph_to_dot(graph: nx.DiGraph, dot_output_path: Path) -> tuple[bool,
     print(f"save_nxgraph_to_dot function's received type: {type(graph).__name__}")
     try:
         nx.drawing.nx_agraph.write_dot(graph, dot_output_path)
+        print(f"Successfully converted nx.DiGraph to .dot type: '{dot_output_path}'")
         return True, dot_output_path
-    except Exception:
-        raise RuntimeError("DOT export requires pydot or pygraphviz.")
+    except Exception as e:
+        #raise RuntimeError("DOT export requires pydot or pygraphviz.")
+        print(f"An error occurred during dot conversion: {e}")
+        return False, None
 
-def save_nxgraph_to_graphml(graph_object, out_path: Path) -> tuple[bool, Path | None]:
+def save_nxgraph_to_graphml(graph_object, graphml_output_path: Path) -> tuple[bool, Path | None]:
     print(f"save_nxgraph_to_graphml function's received type: {type(graph_object).__name__}")
     
     if not isinstance(graph_object, (nx.Graph, nx.DiGraph, nx.MultiGraph, nx.MultiDiGraph)):
         print(f"Conversion Failed: The input object is not a valid NetworkX Graph.")
         print(f"Please convert your data to an nx.Graph object first.")
-        return False, out_path
+        return False, None
 
     try:
-        nx.write_graphml(graph_object, str(out_path))
-        print(f"Successfully converted graph to GraphML file: '{out_path}'")
-        return True, out_path
+        nx.write_graphml(graph_object, str(graphml_output_path))
+        print(f"Successfully converted nx.DiGraph to .graphml type: '{graphml_output_path}'")
+        return True, graphml_output_path
     except Exception as e:
         # This catches errors that occur *during* writing, but 
         # the input check above should prevent the 'is_directed' error.
         print(f"An error occurred during GraphML conversion: {e}")
-        return False, out_path
+        return False, None
 
 def build_cfg(binary_input_path : Path, auto_load_libs=False) -> tuple[angr.Project | angr.analyses.cfg.cfg_fast.CFGFast ] :
     project = angr.Project(binary_input_path, auto_load_libs=auto_load_libs)
     cfg = project.analyses.CFGFast(normalize=True, data_references=True )
     return project, cfg
 
-def elf_to_cfg(binary_path : Path) -> nx.DiGraph:
-
+def elf_to_cfg(binary_path : Path) -> nx.DiGraph | None:
     angr_proj, cfg = build_cfg(binary_path)
-
     cfg_graph = nx.DiGraph()
 
     for node in cfg.graph.nodes():
@@ -58,7 +59,6 @@ def elf_to_cfg(binary_path : Path) -> nx.DiGraph:
     for src, dst, data in cfg.graph.edges(data=True):
         if src.block is None or dst.block is None:
             continue
-
         cfg_graph.add_edge(src.addr, dst.addr, jumpkind=data.get("jumpkind") )
 
     return cfg_graph
